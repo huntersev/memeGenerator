de9a82086b3be29afeeae878194996ef991b1cb6
huntersev <severn.hunter@gmail.com>
2023-02-18 14:21:27 -0500

    modified: app.js index.html

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const imgUrl = document.querySelector('#imgUrl');
+  const imgFile = document.querySelector('#imgFile');
   const topText = document.querySelector('#topText')
   const bottomText = document.querySelector('#bottomText');
-
-  const addMemeBtn = document.querySelector('#addMemeBtn');
-  addMemeBtn.addEventListener('click', addMeme);
-
   const memeDisplay = document.querySelector('#memeDisplay')
+  const addMemeBtn = document.querySelector('#addMemeBtn');
 
-  function addMeme(){
+  let image;
+  
+  //addMemeBtn.addEventListener('click', )
+
+
+  //function addMeme(){
     //if(){ ALL PARAMETERS ARE GOOD
-    const meme = document.createElement('canvas');
+    //const meme = document.createElement('canvas');
     //meme.setAttribute('',); CANVAS BACKGROUND
     //meme.setAttribute('',); CANVAS TOP TEXT
     //meme.setAttribute('',); CANVAS BOTTOM TEXT
     //store in object array?
-    memeDisplay.appendChild(meme);
+    //memeDisplay.appendChild(meme);
     //reset inputs to default value
     //}
 
     //else(){ ALL PARAMETERS ARE NOT GOOD
     //alert('There was a problem with your input');
     //}
-  }
+  //}
 
 })
